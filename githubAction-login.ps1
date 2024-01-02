1b6ed480e8e78e3116503ac7d90a60cedf0986b6
Olivier Miossec <oliviermiossec@hotmail.fr>
2024-01-02 21:14:59 +0100

    test

--- githubAction-login.ps1
+++ githubAction-login.ps1
@@ -5,6 +5,8 @@ $authenticationToken = [System.Convert]::ToBase64String([Text.Encoding]::ASCII.G
         "X-GitHub-Api-Version" = "2022-11-28"
     }
 
+    $headers
+
 $reposAPIUri = "https://api.github.com/repos/omiossec/pwsh-GitHub-API/issues"
 
 $githubRepositories = Invoke-RestMethod -Method get -Uri $reposAPIUri -Headers $headers 
